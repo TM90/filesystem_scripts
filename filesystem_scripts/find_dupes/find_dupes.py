ed1de705d82b988cd56dbc45f8b0b3f0cdf8c272
TM90 <tobias_markus@gmx.net>
2025-10-04 21:43:29 +0200

    initial script to find already backuped files in an unsorted subpath

--- filesystem_scripts/find_dupes/find_dupes.py
+++ filesystem_scripts/find_dupes/find_dupes.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from io import StringIO
 from pathlib import Path
+import sys
 import subprocess
 
 
@@ -16,10 +17,19 @@ def parse_to_dict(raw: str) -> dict[str, list[Path]]:
     return result_dict
 
 
-def create_checksum_filelist(path: Path) -> dict[str, list[Path]]:
-    result = subprocess.check_output(
-        f"find {str(path)} -type f | xargs -d '\n' sha256sum", shell=True
-    )
+def create_checksum_filelist(
+    path: Path, exclude: Path | None = None
+) -> dict[str, list[Path]]:
+    if exclude:
+        result = subprocess.check_output(
+            f"find {str(path)} -type f -not -path {str(exclude)} | xargs -d '\n' sha256sum",
+            shell=True,
+        )
+    else:
+        result = subprocess.check_output(
+            f"find {str(path)} -type f | xargs -d '\n' sha256sum",
+            shell=True,
+        )
     return parse_to_dict(result.decode("utf-8"))
 
 
@@ -32,9 +42,51 @@ def find_dupes(path: Path, _result_page: StringIO) -> None:
             print(f"\t - {file}")
 
 
+def find_set_b_files_already_in_set_a(
+    file_path_dataset_a: Path, file_path_dataset_b: Path
+) -> None:
+    print("running with insertion_folder")
+    filelist = create_checksum_filelist(
+        file_path_dataset_a,
+    )
+    reduced_list = {key: value for key, value in filelist.items() if len(value) > 1}
+
+    for checksum, files in reduced_list.items():
+        matches = len(
+            [file for file in files if str(file).startswith(str(file_path_dataset_b))]
+        )
+        if matches > 0:
+            print(f"{checksum}:")
+            for file in files:
+                print(f"- {str(file)}")
+
+
 if __name__ == "__main__":
-    parser = ArgumentParser(prog="find_dupes", description="list duplicated files")
-    _ = parser.add_argument("filepath")
+    parser = ArgumentParser(description="list duplicated files")
+    _ = parser.add_argument(
+        "filepath",
+        type=Path,
+        help="dataset A in which to search for duplicates",
+    )
+    _ = parser.add_argument(
+        "-I",
+        "--insertion_folder",
+        type=Path,
+        required=False,
+        help="data_set_b which should be inserted in dataset A",
+    )
+    _ = parser.add_argument(
+        "-D",
+        "--delete",
+        help="in insertion mode delete duplicates in data_set_b",
+    )
     args = parser.parse_args()
-    fp = StringIO()
-    find_dupes(Path(Path(args.filepath)), fp)
+    if not args.insertion_folder:
+        print("a")
+        fp = StringIO()
+        find_dupes(args.filepath, fp)
+        sys.exit(0)
+    else:
+        print("b")
+        find_set_b_files_already_in_set_a(args.filepath, args.insertion_folder)
+        sys.exit(0)
